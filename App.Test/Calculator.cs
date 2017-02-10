87163206383caaacd452b917f76027cbec2760a6
gg_phongtv <phongtv@greenglobal.vn>
2017-02-10 13:19:38 +0700

    Test event for control

--- App.Test/Calculator.cs
+++ App.Test/Calculator.cs
@@ -11,7 +11,7 @@ using NUnit.Extensions.Forms;
 namespace App.Test
 {
     [TestFixture(Author ="PhongTV", Description ="Testing for winform")]
-    [Ignore("Ignore this test case when commit because NUNitForms doesn't support when build on Travis")]
+    //[Ignore("Ignore this test case when commit because NUNitForms doesn't support when build on Travis")]
     public class Calculator
     {
         private FrmCalculator frmCalculator;
@@ -27,6 +27,35 @@ namespace App.Test
         public void InitialForm()
         {            
             Assert.AreEqual("Calculator", frmCalculator.Text);
+        }
+
+        [Test]
+        public void InitialControls()
+        {
+            LabelTester num1 = new LabelTester("lblNum1", frmCalculator);
+            LabelTester num2 = new LabelTester("lblNum2", frmCalculator);
+            ButtonTester submitButton = new ButtonTester("btnCal", frmCalculator);
+
+            Assert.AreEqual("Number 1", num1.Text);
+            Assert.AreEqual("Number 2", num2.Text);
+            Assert.AreEqual("Submit", submitButton.Text);
+        }
+
+        [Test]
+        public void TestEvent()
+        {
+            TextBoxTester txtNum1 = new TextBoxTester("txtNum1", frmCalculator);
+            TextBoxTester txtNum2 = new TextBoxTester("txtNum2", frmCalculator);
+            ButtonTester submitButton = new ButtonTester("btnCal", frmCalculator);
+
+            Assert.AreEqual("Submit", submitButton.Text);
+
+            txtNum1.Properties.Text = "4";
+            txtNum2.Properties.Text = "1";
+
+            submitButton.Click();
+            LabelTester lblResult = new LabelTester("lblResult", frmCalculator);
+            Assert.AreEqual("5", lblResult.Text);
         }
     }
 }
